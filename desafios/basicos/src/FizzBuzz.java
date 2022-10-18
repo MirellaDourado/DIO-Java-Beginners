585c7ed13a802ac90520e30f44cc3996326c679d
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-10-18 00:54:36 -0300

    desafio - básico 4 (FizzBuzz)

---
+++ desafios/basicos/src/FizzBuzz.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Informe um número:");
+    int number = scan.nextInt();
+    
+    if(number % 3 == 0 && number % 5 == 0) System.out.println("FizzBuzz");
+    else if(number % 3 == 0) System.out.println("Fizz");
+    else if (number % 5 == 0) System.out.println("Buzz");
+    else System.out.println(number);
+  }
+}
