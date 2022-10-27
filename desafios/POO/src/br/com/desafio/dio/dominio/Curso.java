3677b967fd0a1e727f9a5c9507a1d4b21ae5cc3d
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-10-27 11:46:18 -0300

    adicionando atributos

---
+++ desafios/POO/src/br/com/desafio/dio/dominio/Curso.java
@@ -0,0 +1,9 @@
+package br.com.desafio.dio.dominio;
+
+public class Curso {
+  // Boa prática, nos atributos utilizarmos o modificador de acesso private
+  
+  private String titulo;
+  private String descricao;
+  private int cargaHoraria;
+}
---
+++ desafios/POO/src/br/com/desafio/dio/dominio/Mentoria.java
@@ -0,0 +1,12 @@
+package br.com.desafio.dio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    // Boa prática, nos atributos utilizarmos o modificador de acesso private
+  
+  private String titulo;
+  private String descricao;
+  // LocalDate e LocalTime lidam com datas!
+  private LocalDate data;
+}
