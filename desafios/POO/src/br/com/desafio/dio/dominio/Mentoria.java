235b69459f7f211149f46be1bc97eb16e13dde14
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-10-27 16:37:10 -0300

    getters and setters + toString

--- desafios/POO/src/br/com/desafio/dio/dominio/Mentoria.java
+++ desafios/POO/src/br/com/desafio/dio/dominio/Mentoria.java
@@ -1,5 +1,4 @@
 package br.com.desafio.dio.dominio;
-
 import java.time.LocalDate;
 
 public class Mentoria {
@@ -9,4 +8,39 @@ public class Mentoria {
   private String descricao;
   // LocalDate e LocalTime lidam com datas!
   private LocalDate data;
+
+  public String getTitulo () {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getDescricao () {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+  
+  public LocalDate getData() {
+    return data;
+  }
+  
+  public void setData(LocalDate data) {
+    this.data = data;
+    
+  }
+
+  @Override
+  public java.lang.String toString() {
+    return "Mentoria{" +
+            "titulo='" + titulo + '\'' +
+            ", descricao='" + descricao + '\'' +
+            ", data=" + data +
+            '}';
+  }
 }
+
