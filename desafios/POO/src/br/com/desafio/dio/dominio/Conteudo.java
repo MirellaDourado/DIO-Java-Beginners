cba9fc66d895f930c6f4c8726b2f09dbb1742eb6
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-10-27 23:44:13 -0300

    gets e sets de conteudo

--- desafios/POO/src/br/com/desafio/dio/dominio/Conteudo.java
+++ desafios/POO/src/br/com/desafio/dio/dominio/Conteudo.java
@@ -1,5 +1,33 @@
 package br.com.desafio.dio.dominio;
 
-public class Conteudo {
+// classe abstrata = que dizer que não é possivel instanciar a classe
+public abstract class Conteudo {
+ 
+  //protected = somente as filhas poderam ter acesso
+  // static = essa classe poderá ser acessada fora da classe conteúdo
+  protected static final double XP_PADRAO = 10d;
+
+  private String titulo;
+  private String descricao;
+
+  // abstract = as classes que extendeerem de conteudos serão obrigados a implementar uma logica neste método
+  public abstract double calcularXP();
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
   
 }
